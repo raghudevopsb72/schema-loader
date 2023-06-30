c7c38a6d264ba7f27b789baf06065c8ed9906178
Raghu K <raghuk.vit@gmail.com>
2023-06-30 07:24:15 +0530

    Update schema-loader/

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM          amazon/aws-cli
-RUN           yum install jq -y
+COPY          mongo.repo /etc/yum.repos.d/mongo.repo
+RUN           yum install jq mongodb-org-shell git -y
 COPY          run.sh /
 ENTRYPOINT    [ "bash", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,18 +1,12 @@
-if [ "$SCHEMA_TYPE" == "mongo" ]; then
-
-echo '[mongodb-org-4.2]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-gpgcheck=0
-enabled=1' >/etc/yum.repos.d/mongo.repo
-
-yum install mongodb-org-shell git -y
-
 git clone https://github.com/raghudevopsb72/${COMPONENT}
 cd $COMPONENT
 
-curl -L -O https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem rds-combined-ca-bundle.pem
-mongo --ssl --host ${DOCDB_ENDPOINT}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${DOCDB_USER} --password ${DOCDB_PASS} <schema/${COMPONENT}.js
+if [ "$SCHEMA_TYPE" == "mongo" ]; then
+  DOCDB_ENDPOINT=$(aws ssm get-parameters  --names prod.docdb.db_endpoint --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  DOCDB_USER=$(aws ssm get-parameters  --names prod.docdb.db_user --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  DOCDB_PASS=$(aws ssm get-parameters  --names prod.docdb.db_pass --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  curl -L -O https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem rds-combined-ca-bundle.pem
+  mongo --ssl --host ${DOCDB_ENDPOINT}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${DOCDB_USER} --password ${DOCDB_PASS} <schema/${COMPONENT}.js
 
 fi
 
