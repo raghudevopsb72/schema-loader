c84ed77e05ce449dd03590ed22ac6e5f03b56c00
Raghu K <raghuk.vit@gmail.com>
2023-06-30 07:41:04 +0530

    Update schema-loader/

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ "$SCHEMA_TYPE" == "mongo" ]; then
   DOCDB_ENDPOINT=$(aws ssm get-parameters  --names prod.docdb.db_endpoint --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
   DOCDB_USER=$(aws ssm get-parameters  --names prod.docdb.db_user --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
   DOCDB_PASS=$(aws ssm get-parameters  --names prod.docdb.db_pass --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
-  curl -L -O https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem rds-combined-ca-bundle.pem
+  curl -L https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem -o rds-combined-ca-bundle.pem
   mongo --ssl --host ${DOCDB_ENDPOINT}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${DOCDB_USER} --password ${DOCDB_PASS} <schema/${COMPONENT}.js
 
 fi
